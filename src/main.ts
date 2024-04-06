fb8461c2a3e67f847b3c69b6716daac01a2a4558
Surawee kraikruan <67056659+sura1542@users.noreply.github.com>
2024-04-06 23:05:33 +0700

    test1

--- src/main.ts
+++ src/main.ts
@@ -13,31 +13,31 @@
 // Note: Don't allow to use any sort function
 
 
-export function PRIMO(collection_1: number[], collection_2: number[], collection_3: number[]): number[] {
+export function PRIMO(num1: number[], num2: number[], num3: number[]): number[] {
     const result: number[] = [];
     let i = 0;
     let j = 0;
     let k = 0;
 
-    while (i < collection_1.length && j < collection_2.length) {
-        if (collection_1[i] < collection_2[j]) {
-            result[k] = collection_1[i];
+    while (i < num1.length && j < num2.length) {
+        if (num1[i] < num2[j]) {
+            result[k] = num1[i];
             i++;
         } else {
-            result[k] = collection_2[j];
+            result[k] = num2[j];
             j++;
         }
         k++;
     }
 
-    while (i < collection_1.length) {
-        result[k] = collection_1[i];
+    while (i < num1.length) {
+        result[k] = num2[i];
         i++;
         k++;
     }
 
-    while (j < collection_2.length) {
-        result[k] = collection_2[j];
+    while (j < num2.length) {
+        result[k] = num2[j];
         j++;
         k++;
     }
--- tests/main.test.ts
+++ tests/main.test.ts
@@ -2,12 +2,12 @@ import { PRIMO } from '../src/main';
 
 describe('test', () => {
     it('should merge three sorted arrays correctly', () => {
-        const collection1 = [1, 3, 5, 7];
-        const collection2 = [8, 6, 4, 2];
-        const collection3 = [0, 2, 4, 6, 8];
+        const num1 = [1, 3, 5, 7];
+        const num2 = [8, 6, 4, 2];
+        const num3 = [0, 2, 4, 6, 8];
 
         const expected = [0, 1, 2, 2, 3, 4, 4, 5, 6, 6, 7, 8, 8];
-        const result = PRIMO(collection1, collection2, collection3);
+        const result = PRIMO(num1, num2, num3);
 
         expect(result).toEqual(expected);
     });
